757cde0ae037dd68a903e763901060e42f8b27cd
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2024-12-09 20:47:13 -0300

    .

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,9 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyFirstApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] //acessa o endpoint por essa rota
+    [ApiController] //precisa colocar para identificar que é um controller
     public class UserController : ControllerBase
     {
+        //controllerbase = herança, atraves dele acessar funções importantes
+        
+        //IActionResult = endpoint sempre devolve essa interface
+        [HttpGet]//declarando o tipo da função
+        //[Route("{id}")] //passando informação pela rota (path)
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]//especificando os tipos de respostas que cada endpoint pode retornar
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        //public IActionResult Get(int id) //passando parametros pela url (query string)
+        public IActionResult Get([FromHeader]int id, [FromHeader] string nickname) //[] especificando de onde o valor vem, se não colocar nada ele vem da query string, para vir do header precisa especificar
+        {
+            var response = new User
+            {
+                Id = 1,
+                Age = 10,
+                Name = "Alice"
+            };
+
+            return Ok(response);
+            //ok(); = função que devolve um ok
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); //
 
 // Add services to the container.
 
@@ -7,19 +7,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRouting(option => option.LowercaseUrls = true);//forçando as urls em lowercase
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{   
+    //se for desenvolvimento chama o swagger
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//redireciona para https
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+//mapeamento dos controlers 
 app.MapControllers();
 
 app.Run();
